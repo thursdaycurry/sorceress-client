d1c9db1eb649d455e8ab83182892a13f76a8009a
thursdaycurry <thursdaycurry@gmail.com>
2024-02-15 23:58:07 +0900

    project init

---
+++ src/components/CountryTable.js
@@ -0,0 +1,61 @@
+import { useEffect, useState } from 'react';
+
+function CountryTable() {
+  const [countries, setCountries] = useState([]);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(`http://localhost:3000/country`);
+
+        console.log(response);
+        if (!response.ok) {
+          throw new Error(`fail to fetch`);
+        }
+        const data = await response.json();
+        setCountries(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {isLoading && <p>Loading...</p>}
+      {error && <p> Error: {error}</p>}
+
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Capital</th>
+            <th>Region</th>
+            <th>Population</th>
+          </tr>
+        </thead>
+        <tbody>
+          {countries.map((country) => (
+            <tr key={country.code}>
+              <td>{country.name}</td>
+              <td>{country.code}</td>
+              <td>{country.region}</td>
+              <td>{country.sub_region}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default CountryTable;

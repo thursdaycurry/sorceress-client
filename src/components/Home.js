045788cea1f9d62b084083ded9e92e32fdd470b5
Heegu Yang <87453411+thursdaycurry@users.noreply.github.com>
2024-02-16 17:29:29 +0900

    환경변수 노출 제거 (#5)

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ const Home = () => {
     <div>
       <h2>Home Page</h2>
       <p>Welcome :D</p>
-      {process.env.REACT_APP_HOST}
+      <div>(test)delete this: {process.env.REACT_APP_HOST.substr(0, 4)}</div>
     </div>
   );
 };

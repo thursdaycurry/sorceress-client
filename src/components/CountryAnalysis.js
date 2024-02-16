27a76624a7d1156c42278b172172b2d6654b3577
thursdaycurry <thursdaycurry@gmail.com>
2024-02-16 16:50:31 +0900

    energy 분석 컴포넌트 추가

---
+++ src/components/CountryAnalysis.js
@@ -0,0 +1,12 @@
+import AnalReqEnergy from './AnalReqEnergy';
+import CountrySelector from './CountrySelector';
+
+const CountryAnalysis = () => {
+  return (
+    <div>
+      <CountrySelector />
+      <AnalReqEnergy />
+    </div>
+  );
+};
+export default CountryAnalysis;
